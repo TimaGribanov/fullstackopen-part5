c36db40d132fb2108106fe37e6df414937c86e8a
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2024-02-14 12:52:28 +0100

    5.12 done

---
+++ cypress/e2e/blog_app.cy.js
@@ -0,0 +1,35 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      username: 'tester-cypress', password: 'testing-cy', name: 'Tester Cypress'
+    })
+    cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('log in').click()
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('log in').click()
+
+      cy.get('#username').type('tester-cypress')
+      cy.get('#password').type('testing-cy')
+      cy.get('#login-button').click()
+
+      cy.contains('Tester Cypress logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('log in').click()
+
+      cy.get('#username').type('wrong')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.contains('Wrong credentials')
+    })
+  })
+})
